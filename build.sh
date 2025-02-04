49e88baf5e8c07b424ad3cad144f6b4bda3307ba
ZeStig <zestig@duck.com>
2025-02-04 17:37:50 +0000

    Changes

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-command -v apt >/dev/null 2>&1 && sudo apt update && sudo apt install -y bzip2 git vim make gcc libncurses-dev flex bison bc cpio libelf-dev libssl-dev
+command -v apt >/dev/null 2>&1 && apt update && apt install -y sudo wget xz-utils bzip2 git vim make gcc libncurses-dev flex bison bc cpio libelf-dev libssl-dev syslinux isolinux genisoimage
 
-cd src/
+mkdir -p /data/src && cd /data/src/
+
 wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.12.10.tar.xz
 tar xf linux-6.12.10.tar.xz
 
@@ -20,7 +21,8 @@ cd ../../out/initramfs/
 find . | cpio -o -H newc > init.cpio
 
 
-cd src/linux-6.12.10/
+cd ../../src/linux-6.12.10/
+# make
 make -j $(nproc) isoimage FDARGS="initrd=/init.cpio" FDINITRD=../../out/initramfs/init.cpio
 
-cp src/linux-6.12.10/arch/x86_64/
+# docker cp it to out/
