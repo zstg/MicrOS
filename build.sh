7ab1bc65158ee20104d18a810de7fcc8c5c35ade
ZeStig <zestig@duck.com>
2025-02-04 17:45:04 +0000

    Modify build.sh to use absolute paths

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# docker run --rm --privileged -v ./:/data -it ubuntu bash
 command -v apt >/dev/null 2>&1 && apt update && apt install -y sudo wget xz-utils bzip2 git vim make gcc libncurses-dev flex bison bc cpio libelf-dev libssl-dev syslinux isolinux genisoimage
 
 mkdir -p /data/src && cd /data/src/
@@ -9,20 +10,17 @@ tar xf linux-6.12.10.tar.xz
 wget https://busybox.net/downloads/busybox-1.37.0.tar.bz2
 tar xf busybox-1.37.0.tar.bz2
 
-cd ..
-cp kernel.config src/linux-6.12.10/.config
-cp busybox.config src/busybox-1.37.0/.config
+cp ../kernel.config /data/src/linux-6.12.10/.config
+cp ../busybox.config /data/src/busybox-1.37.0/.config
 
-cd src/busybox-1.37.0/
+cd /data/src/busybox-1.37.0/
 make
-make CONFIG_PREFIX=../../out/initramfs install # this "splits" the busybox install into individual scripts
+make CONFIG_PREFIX=/data/out/initramfs install # this "splits" the busybox install into individual scripts
+rm /data/out/initramfs/linuxrc
 
-cd ../../out/initramfs/
-find . | cpio -o -H newc > init.cpio
+cd /data/out/initramfs/
+echo init | cpio -o -H newc > init.cpio
 
-
-cd ../../src/linux-6.12.10/
+cd /data/src/linux-6.12.10/
 # make
-make -j $(nproc) isoimage FDARGS="initrd=/init.cpio" FDINITRD=../../out/initramfs/init.cpio
-
-# docker cp it to out/
+make -j $(nproc) isoimage FDARGS="initrd=/init.cpio" FDINITRD=/data/out/initramfs/init.cpio
