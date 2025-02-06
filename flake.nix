18941c22b8e0bcf9e867dfacefbe9b813d8e3fe9
kantonato04 <kantonato04@gmail.com>
2025-02-06 14:20:43 +0000

    Use NixOS's static busybox package in the flake

--- flake.nix
+++ flake.nix
@@ -21,61 +21,55 @@
 
         busyboxSrc = pkgs.fetchurl {
           url = "https://busybox.net/downloads/busybox-1.37.0.tar.bz2";
-          sha256 = "19gs46585jr98ghqnnh6blr748zj3phc71yxfbzpsl869mqn2cdl";
+          sha256 = "sha256-MxHf8y50ZJn03w1d8E1+s5Y4LX4Qi7klDntRm4NwQ6Q=";
         };
 
         buildKernel = pkgs.stdenv.mkDerivation {
           name = "custom-linux-kernel";
           src = linuxSrc;
-          nativeBuildInputs = with pkgs; [
-            bc bison flex gcc elfutils ncurses openssl
-          ];
+          nativeBuildInputs = with pkgs; [ bc bison flex gcc elfutils ncurses openssl sudo ];
 
           buildPhase = ''
-            make tinyconfig
-            make 
+            cp ${./kernel.config} .config
+            make -j $(nproc)
           '';
 
           installPhase = ''
-            mkdir -p $out/home/user/MicrOS
-            cp arch/x86/boot/bzImage $out/home/user/MicrOS
+            mkdir -p $out/MicrOS
+            cp arch/x86/boot/bzImage $out/MicrOS
           '';
         };
 
         buildBusybox = pkgs.stdenv.mkDerivation {
           name = "custom-busybox";
-          src = busyboxSrc;
-          buildInputs = with pkgs; [ ncurses ];
-
-          buildPhase = ''
-            make CONFIG_STATIC=y
-          '';
-
+          unpackPhase = ":";
+          dontUnpack = true;
+          dontConfigure = true;
+          dontBuild = true;
           installPhase = ''
-            mkdir -p $out/initramfs
-            make CONFIG_PREFIX=$out/initramfs install
-          '';
+                mkdir -p $out/MicrOS/initramfs/bin/
+                cp -r ${pkgs.pkgsStatic.busybox}/* $out/MicrOS/initramfs/
+                rm -rf $out/MicrOS/initramfs/{default.script,linuxrc}
+                
+                #echo -e "#!/bin/sh\n\n/bin/sh" > $out/MicrOS/initramfs/init
+                #chmod +x $out/MicrOS/initramfs/init
+                ln -sf $out/MicrOS/initramfs/bin/sh $out/MicrOS/initramfs/init
+            '';
         };
 
-        createInitramfs = pkgs.writeShellScriptBin "create-initramfs" ''
-          # Create initramfs
-          cd ${buildBusybox}/initramfs
-          rm linuxrc
-          chmod +x init
-          
-          # Create bootable image
-          dd if=/dev/zero of=boot.img bs=1M count=50
-          mkfs.vfat boot.img
-          mkdir mnt
-          mount boot.img mnt
-          
-          cp ${buildKernel}/boot/bzImage mnt/
-          
+        createInitramfs = pkgs.writeShellScriptBin "create-initramfs" ''     
+          cd $out/MicrOS/initramfs
+          find . | cpio -o -H newc > ../init.cpio   
           cd ..
-          find initramfs | cpio -o -H newc > mnt/init.cpio
-          umount mnt
-          
-          echo "Bootable image created: $(pwd)/boot.img"
+          dd if=/dev/zero of=boot bs=1M count=50
+          mkfs -t fat boot
+
+          rm -rf m
+          syslinux boot
+          mkdir m
+          mount boot m
+          cp bzImage init.cpio m
+          umount m
         '';
 
       in {
