c61c717757fe29597381424553b167d1c8aaf12d
zstg <o0vckutt@duck.com>
2025-02-06 21:49:14 +0530

    Changes

--- flake.nix
+++ flake.nix
@@ -1,5 +1,4 @@
-{
-  description = "Custom Linux kernel development environment";
+{ description = "Custom Linux kernel development environment";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -16,7 +15,7 @@
         
         linuxSrc = pkgs.fetchurl {
           url = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.6.75.tar.xz";
-          sha256 = "sha256-99+x+pcWuhOdC0yBYVNYFtQA3qIdWUP1E0SEKbF5ApA=";  # nix-prefetch-url --unpack "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.6.75.tar.xz"
+          sha256 = "sha256-99+x+pcWuhOdC0yBYVNYFtQA3qIdWUP1E0SEKbF5ApA=";
         };
 
         buildKernel = pkgs.stdenv.mkDerivation {
@@ -33,53 +32,64 @@
             mkdir -p $out/MicrOS
             cp arch/x86/boot/bzImage $out/MicrOS
           '';
+
+          meta.description = "Custom Linux kernel compilation";
         };
 
         buildBusybox = pkgs.stdenv.mkDerivation {
           name = "custom-busybox";
           nativeBuildInputs = with pkgs; [ elfutils cpio util-linux dosfstools syslinux ];
-          unpackPhase = ":";
+          
+          phases = ["installPhase"];
           dontUnpack = true;
           dontConfigure = true;
           dontBuild = true;
+
           installPhase = ''
-                mkdir -p $out/MicrOS/initramfs/bin/
-                cp -r ${pkgs.pkgsStatic.busybox}/* $out/MicrOS/initramfs/
-                rm -rf $out/MicrOS/initramfs/{default.script,linuxrc}
-                
-                #echo -e "#!/bin/sh\n\n/bin/sh" > $out/MicrOS/initramfs/init
-                #chmod +x $out/MicrOS/initramfs/init
-                ln -sf $out/MicrOS/initramfs/bin/sh $out/MicrOS/initramfs/init
+            mkdir -p $out/MicrOS/initramfs/bin/
+            
+            # Create proper directory structure
+            mkdir -p $out/MicrOS/initramfs
+            
+            # Copy static busybox binaries
+            cp -r ${pkgs.pkgsStatic.busybox}/* $out/MicrOS/initramfs/
+            
+            # Remove unwanted files
+            rm -rf $out/MicrOS/initramfs/{default.script,linuxrc}
+            
+            # Create init symlink
+            ln -sf $out/MicrOS/initramfs/bin/sh $out/MicrOS/initramfs/init
+            
+            # Create initramfs archive
+            cd $out/MicrOS/initramfs
+            find . | cpio -o -H newc > ../init.cpio
+            
+            # Print contents for verification
+            cd ..
+            dd if=/dev/zero of=boot bs=1M count=50
 
-                cd $out/MicrOS/initramfs
-                find . | cpio -o -H newc > ../init.cpio   
-                cd ..
 
-                dd if=/dev/zero of=boot.img bs=1M count=50
-                mkfs -t fat boot.img
-                syslinux boot.img
+            mkfs -t fat boot
+            syslinux boot
 
-                mount boot.img m/
-                cp bzImage init.cpio m/
-                umount m/
-
-                # run `qemu-system-x86_64 boot.img
-                # enter /bzImage -initrd=/init.cpio in the `boot:` prompt
-
+            mkdir m
+            mount boot m
           '';
+
+          meta.description = "Custom BusyBox-based initramfs creation";
         };
 
         run-qemu = pkgs.writeShellScriptBin "run-qemu" ''
-      exec ${pkgs.qemu_kvm}/bin/qemu-system-x86_64 \
-        -enable-kvm \
-        -m 2048 \
-        -cpu host \
-        -drive file=${buildBusybox}/MicrOS/boot.img,format=raw \
-        -serial stdio \
-        -monitor stdio \
-        -vnc :0 \
-        "$@"
-    '';
+          exec ${pkgs.qemu_kvm}/bin/qemu-system-x86_64 \
+            -enable-kvm \
+            -m 2048 \
+            -cpu host \
+            -drive file=${buildBusybox}/MicrOS/boot,format=raw \
+            -serial stdio \
+            -monitor stdio \
+            -vnc :0 \
+            "$@"
+        '';
 
       in {
         packages = {
@@ -94,9 +104,9 @@
             syslinux dosfstools vim cpio
           ];
           shellHook = ''
-          echo "Custom kernel development environment initialized."
-          echo "Run 'qemu-runner' to start the VM."
-      '';
+            echo "Custom kernel development environment initialized."
+            echo "Run 'qemu-runner' to start the VM."
+          '';
         };
       }
     );
