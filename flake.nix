de6cf9669f39d97c042d4ca0949accb37e7ecf5d
zstg <o0vckutt@duck.com>
2025-03-07 13:29:18 +0530

    Changes

--- flake.nix
+++ flake.nix
@@ -3,48 +3,44 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, utils }: 
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [];
-        };
-        
-        linuxSrc = pkgs.fetchurl {
-          url = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.6.75.tar.xz";
-          sha256 = "sha256-99+x+pcWuhOdC0yBYVNYFtQA3qIdWUP1E0SEKbF5ApA=";
-        };
+    let
+      system = "x86-64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      
+      linuxSrc = pkgs.fetchurl {
+        url = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.6.75.tar.xz";
+        sha256 = "sha256-99+x+pcWuhOdC0yBYVNYFtQA3qIdWUP1E0SEKbF5ApA=";
+      };
 
-        buildKernel = pkgs.stdenv.mkDerivation {
-          name = "custom-linux-kernel";
-          src = linuxSrc;
-          nativeBuildInputs = with pkgs; [ bc bison flex gcc ncurses openssl ];
+      buildKernel = pkgs.stdenv.mkDerivation {
+        name = "custom-linux-kernel";
+        src = linuxSrc;
+        nativeBuildInputs = with pkgs; [ bc bison flex gcc ncurses openssl ];
 
-          buildPhase = ''
+        buildPhase = ''
             cp ${./kernel.config} .config
             make -j $(nproc)
           '';
 
-          installPhase = ''
+        installPhase = ''
             mkdir -p $out/MicrOS
             cp arch/x86/boot/bzImage $out/MicrOS
           '';
 
-          meta.description = "Custom Linux kernel compilation";
-        };
+        meta.description = "Custom Linux kernel compilation";
+      };
 
-        buildBusybox = pkgs.stdenv.mkDerivation {
-          name = "custom-busybox";
-          nativeBuildInputs = with pkgs; [ elfutils cpio util-linux dosfstools syslinux sudo];
-          phases = ["installPhase"];
-          dontUnpack = true;
-          dontConfigure = true;
-          dontBuild = true;
-          installPhase = ''
+      buildBusybox = pkgs.stdenv.mkDerivation {
+        name = "custom-busybox";
+        nativeBuildInputs = with pkgs; [ elfutils cpio util-linux dosfstools syslinux sudo];
+        phases = ["installPhase"];
+        dontUnpack = true;
+        dontConfigure = true;
+        dontBuild = true;
+        installPhase = ''
             mkdir -p $out/MicrOS/initramfs/bin/
             cp -r ${pkgs.pkgsStatic.busybox}/* $out/MicrOS/initramfs/
 
@@ -63,13 +59,14 @@
             syslinux boot
             sudo mount --mkdir boot m # mounts don't work inside the chroot?!
           '';
-        };
+      };
 
-        run-qemu = pkgs.writeShellScriptBin "run-qemu" ''
+      run-qemu = pkgs.writeShellScriptBin "run-qemu" ''
           exec ${pkgs.qemu_kvm}/bin/qemu-system-x86_64 ${buildBusybox}/MicrOS/boot
         '';
 
-      in {
+    in
+      {
         packages = {
           kernel = buildKernel;
           busybox = buildBusybox;
@@ -86,6 +83,5 @@
             echo "Run 'qemu-runner' to start the VM."
           '';
         };
-      }
-    );
+      };
 }
