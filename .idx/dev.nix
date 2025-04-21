cf9ceaea46adf1711a94097b5f0423ded4ad85f7
ZeStig <69384921+zstg@users.noreply.github.com>
2025-04-21 15:51:42 +0530

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -26,6 +26,10 @@
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       # "vscodevim.vim"
+      "Google.geminicodeassist"
+      "stateful.runme"
+      "vscode-icons-team.vscode-icons"
+      "vscode-org-mode.org-mode"
     ];
 
     # Enable previews
