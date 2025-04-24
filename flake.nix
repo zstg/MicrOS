d89beb2a811af878abf13c0503beaf05a8967419
zstg <o0vckutt@duck.com>
2025-04-24 20:59:28 +0530

    Update flake

--- flake.nix
+++ flake.nix
@@ -46,100 +46,94 @@
       enableParallelBuilding = true;
     };
 
-myStaticApp = pkgs.pkgsStatic.stdenv.mkDerivation {
-  pname = "my-static-app";
-  version = "1.0";
+    myStaticApp = pkgs.pkgsStatic.stdenv.mkDerivation {
+      pname = "my-static-app";
+      version = "1.0";
 
-  src = srccode;
-  nativeBuildInputs = with pkgs;[ musl gcc ];
+      src = srccode;
+      nativeBuildInputs = with pkgs; [ musl gcc ];
 
-  unpackPhase = ''
-    mkdir source
-    cp ${srccode} source/hello.c
-    cd source
-  '';
+      unpackPhase = ''
+        mkdir source
+        cp ${srccode} source/hello.c
+        cd source
+      '';
 
-  buildPhase = ''
-    musl-gcc -static hello.c -o hello
-  '';
+      buildPhase = ''
+        musl-gcc -static hello.c -o hello
+      '';
 
-  installPhase = ''
-    mkdir -p $out/bin
-    cp hello $out/bin/
-  '';
-};
+      installPhase = ''
+        mkdir -p $out/bin
+        cp hello $out/bin/
+      '';
+    };
 
+    initramfsImage = pkgs.stdenv.mkDerivation {
+      name = "micros-initramfs";
 
-    finaliseBuild = pkgs.writeShellApplication {
-      name = "finaliseBuild";
+      buildInputs = with pkgs; [ coreutils findutils cpio gzip ];
 
-      runtimeInputs = with pkgs; [
-        nix coreutils findutils cpio gzip bash
-      ];
+      phases = [ "buildPhase" ];
 
-      text = ''
-        OUTDIR="$PWD/result"
-        rm -rf "$OUTDIR"
-        mkdir -p "$OUTDIR/initramfs"
+      buildPhase = ''
+        mkdir -p $out/initramfs/{bin,sbin,dev,proc,sys}
 
-        echo "Building custom kernel..."
-        nix build .#customKernel -o result-kernel
+        echo "Copying busybox..."
+        cp -r ${staticBusybox}/bin/* $out/initramfs/bin/
 
-        echo "Building static busybox..."
-        nix build .#staticBusybox -o result-busybox
+        echo "Copying static app..."
+        cp ${myStaticApp}/bin/hello $out/initramfs/bin/
 
-        echo "Building static C app..."
-        nix build .#myStaticApp -o result-myapp
-
-        echo "Preparing initramfs contents..."
-        mkdir -p "$OUTDIR/initramfs/bin"
-        mkdir -p "$OUTDIR/initramfs/sbin"
-        mkdir -p "$OUTDIR/initramfs/dev"
-        mkdir -p "$OUTDIR/initramfs/proc"
-        mkdir -p "$OUTDIR/initramfs/sys"
-
-        cp result-busybox/bin/* "$OUTDIR/initramfs/bin/"
-        cp result-myapp/bin/hello "$OUTDIR/initramfs/bin/"
-
-        echo -e '#!/bin/sh\nmount -t proc proc /proc\nexec /bin/sh' > "$OUTDIR/initramfs/init"
-        chmod +x "$OUTDIR/initramfs/init"
+        echo "Creating init script..."
+        cat > $out/initramfs/init <<EOF
+#!/bin/sh
+mount -t proc proc /proc
+exec /bin/sh
+EOF
+        chmod +x $out/initramfs/init
 
         echo "Packing initramfs..."
-        cd "$OUTDIR/initramfs"
-        find "$OUTDIR"/initramfs/bin -type f -exec chmod +x {} \;
-        find . | cpio -o -H newc | gzip > "$OUTDIR/initramfs.cpio.gz"
+        cd $out/initramfs
+        find . | cpio -o -H newc | gzip > $out/initramfs.cpio.gz
         cd -
 
-        echo "Copying kernel..."
-        cp result-kernel/bzImage "$OUTDIR/"
+        # ln -s $out/initramfs.cpio.gz $out/
+      '';
+    };
 
-        echo "Build complete!"
-        echo "Kernel: $OUTDIR/bzImage"
-        echo "Initramfs: $OUTDIR/initramfs.cpio.gz"
+    runMicrOS = pkgs.writeShellApplication {
+      name = "runMicrOS";
 
-        echo "Testing: qemu-system-x86_64 -kernel $OUTDIR/bzImage -initrd $OUTDIR/initramfs.cpio.gz -m 2048"
-        qemu-system-x86_64 -kernel "$OUTDIR"/bzImage -initrd "$OUTDIR"/initramfs.cpio.gz -m 2048
+      runtimeInputs = with pkgs; [ qemu ];
+
+      text = ''
+        echo "Running MicrOS with QEMU..."
+        qemu-system-x86_64 \
+          -kernel ${customKernel}/bzImage \
+          -initrd ${initramfsImage}/initramfs.cpio.gz \
+          -m 2048
       '';
     };
 
   in {
-    defaultPackage.x86_64-linux = finaliseBuild;
+    defaultPackage.x86_64-linux = runMicrOS;
 
     packages.x86_64-linux = {
       customKernel = customKernel;
       staticBusybox = staticBusybox;
       myStaticApp = myStaticApp;
-      finaliseBuild = finaliseBuild;
+      initramfsImage = initramfsImage;
+      runMicrOS = runMicrOS;
     };
 
-    apps.x86_64-linux.finaliseBuild = {
+    apps.x86_64-linux.runMicrOS = {
       type = "app";
-      program = "${finaliseBuild}/bin/finaliseBuild";
+      program = "${runMicrOS}/bin/runMicrOS";
     };
 
     devShells.x86_64-linux.default = pkgs.mkShell {
-      packages = [ finaliseBuild ];
+      packages = [ runMicrOS ];
     };
   };
 }
-
